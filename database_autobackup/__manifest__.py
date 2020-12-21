39b400e9540a054b0d19e1ab0672b3a8a2a6417a
Khubab <khabab@qutechs.com>
2020-12-21 15:54:42 +0200

    add thumbnails image

--- database_autobackup/__manifest__.py
+++ database_autobackup/__manifest__.py
@@ -18,6 +18,7 @@ Database Auto Backup
         'data/autobackup_data.xml',
         'views/autobackup_config_settings_views.xml'
     ],
+    'images': ['static/description/thumbnails.jpg'],
     'installable': True,
     'application': False,
     'auto_install': False,
